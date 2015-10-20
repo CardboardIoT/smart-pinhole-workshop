a76dfe136ebdf42ad62983b67738e1df412214b4
Ezo Saleh <ezo@the-experimenters.com>
2015-10-20 15:08:05 +0100

    Added base infrastructure.

---
+++ smart-pinhole-workshop.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const workshopper = require('workshopper')
+    , path        = require('path');
+
+function fpath (f) {
+  return path.join(__dirname, f)
+}
+
+workshopper({
+    name        : 'smart-pinhole-workshop'
+  , title       : 'Smart Pinhole Camera IoT Workshop'
+  , subtitle    : '\x1b[23mSelect an exercise and hit \x1b[3mEnter\x1b[23m to begin'
+  , exerciseDir : fpath('./exercises/')
+  , appDir      : __dirname
+  , helpFile    : fpath('help.txt')
+  , menuItems   : []
+  , menu        : {fg: 'black', bg: /^win/.test(process.platform) ? 'yellow' : 220}
+});
