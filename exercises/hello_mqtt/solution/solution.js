fb60fca6ab191d5f9cb55d17e4415fcd2cf1c1b4
Ezo Saleh <ezo@the-experimenters.com>
2015-10-21 07:56:18 +0100

    Basic hello mqtt exercise.

---
+++ exercises/hello_mqtt/solution/solution.js
@@ -0,0 +1,18 @@
+var mqtt = require('mqtt');
+var helloTopic = 'ciot/hello/world';
+
+var client  = mqtt.connect({
+  host: 'test.mosquitto.org',
+  port: '1883'
+});
+
+client.on('connect', function () {
+  client.subscribe(helloTopic);
+  client.publish(helloTopic, "Hello mqtt");
+});
+
+client.on('message', function (topic, payload) {
+  var message = payload.toString();
+  console.log('Incoming message[' + message + ']');
+  client.end();
+});
